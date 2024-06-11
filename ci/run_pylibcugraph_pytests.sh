f19fc8c96bd5b530855912a5ca803881cb04d66d
Alexandria Barghi <abarghi@nvidia.com>
2024-06-11 13:29:56 -0700

    remove build/ci for packages

--- ci/run_pylibcugraph_pytests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2024, NVIDIA CORPORATION.
-
-set -euo pipefail
-
-# Support invoking run_pylibcugraph_pytests.sh outside the script directory
-cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/pylibcugraph/pylibcugraph
-
-pytest --cache-clear "$@" tests
--- ci/test_wheel_pylibcugraph.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
-
-set -eoxu pipefail
-
-./ci/test_wheel.sh pylibcugraph python/pylibcugraph
