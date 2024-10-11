66bcde6b09b4e0bdcaab5ffa262218f2650093cc
Alexandria Barghi <abarghi@nvidia.com>
2024-10-11 13:54:09 -0700

    set cuda version 12.5

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -54,16 +54,17 @@ fi
 
 cd "${package_dir}"
 
-python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+WHEEL_BUILD_ARGS = "-m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check"
 
 # pure-python packages should be marked as pure, and not have auditwheel run on them.
 if [[ ${package_name} == "cugraph-dgl" ]] || \
    [[ ${package_name} == "cugraph-pyg" ]]; then
+    python ${WHEEL_BUILD_ARGS}
     RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 dist
 else
     # Hardcode the output dir
     SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;-DBUILD_SHARED_LIBS=OFF;-DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE;-DCUDA_STATIC_RUNTIME=ON;-DWHOLEGRAPH_BUILD_WHEELS=ON" \
-    python -m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check
+    python ${WHEEL_BUILD_ARGS}
 
     mkdir -p final_dist
     python -m auditwheel repair \
