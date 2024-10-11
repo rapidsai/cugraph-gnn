7676b5d4c8e3c00132193e25c99dff5b3807f9dc
Alexandria Barghi <abarghi@nvidia.com>
2024-10-11 14:42:37 -0700

    remove unused cmake args

--- conda/recipes/pylibwholegraph/build.sh
+++ conda/recipes/pylibwholegraph/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022-2024, NVIDIA CORPORATION.
 
-CMAKE_EXTRA_ARGS="--cmake-args=\"-DBUILD_OPS_WITH_TORCH_C10_API=OFF\""
-
-./build.sh pylibwholegraph --allgpuarch -v ${CMAKE_EXTRA_ARGS}
+./build.sh pylibwholegraph --allgpuarch -v
