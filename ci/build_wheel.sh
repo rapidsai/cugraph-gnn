065c5df2eda3900069ebacd297fd7846012fc8cc
James Lamb <jlamb@nvidia.com>
2024-10-15 12:13:23 -0500

    add pytest-forked, try to fix wheel-testing scripts

--- ci/build_wheel.sh
+++ ci/build_wheel.sh
@@ -16,13 +16,13 @@ RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
 cd "${package_dir}"
 
-WHEEL_BUILD_ARGS="-m pip wheel . -w dist -vvv --no-deps --disable-pip-version-check"
+WHEEL_BUILD_ARGS="-m pip wheel . -w dist -v --no-deps --disable-pip-version-check"
 
 # pure-python packages should be marked as pure, and not have auditwheel run on them.
 if [[ ${package_name} == "cugraph-dgl" ]] || \
    [[ ${package_name} == "cugraph-pyg" ]]; then
     python ${WHEEL_BUILD_ARGS}
-    RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 dist
+    RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" RAPIDS_PY_WHEEL_PURE="1" rapids-upload-wheels-to-s3 python dist
 else
     # Hardcode the output dir
     SKBUILD_CMAKE_ARGS="-DDETECT_CONDA_ENV=OFF;-DBUILD_SHARED_LIBS=OFF;-DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE;-DCUDA_STATIC_RUNTIME=ON;-DWHOLEGRAPH_BUILD_WHEELS=ON" \
@@ -30,9 +30,10 @@ else
 
     mkdir -p final_dist
     python -m auditwheel repair \
-    --exclude libcuda.so.1 \
-    --exclude libnvidia-ml.so.1 \
-    -w final_dist dist/*
+        --exclude libcuda.so.1 \
+        --exclude libnvidia-ml.so.1 \
+        -w final_dist \
+        dist/*
 
     RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-upload-wheels-to-s3 final_dist
 fi
--- ci/test_wheel.sh
+++ ci/test_wheel.sh
@@ -11,9 +11,9 @@ mkdir -p ./dist
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 RAPIDS_PY_WHEEL_NAME="${package_name}_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 ./dist
 
-# use 'ls' to expand wildcard before adding `[extra]` requires for pip
-# pip creates wheels using python package names
-python -m pip install $(ls ./dist/${python_package_name}*.whl)[test]
+# echo to expand wildcard before adding `[extra]` requires for pip
+python -m pip install \
+    "$(echo ./dist/${python_package_name}*.whl)[test]"
 
 # Run smoke tests for aarch64 pull requests
 arch=$(uname -m)
