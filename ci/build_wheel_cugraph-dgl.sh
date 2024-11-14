2dd300122dfd6fdea70c9d20c276a3c5946b7613
James Lamb <jlamb@nvidia.com>
2024-11-14 17:26:27 -0600

    enforce wheel size limits, README formatting in CI (#67)
    Contributes to https://github.com/rapidsai/build-planning/issues/110
    
    Proposes adding 2 types of validation on wheels in CI, to ensure we continue to produce wheels that are suitable for PyPI.
    
    * checks on wheel size (compressed),
      - *to be sure they're under PyPI limits*
      - *and to prompt discussion on PRs that significantly increase wheel sizes*
    * checks on README formatting
      - *to ensure they'll render properly as the PyPI project homepages*
      - *e.g. like how https://github.com/scikit-learn/scikit-learn/blob/main/README.rst becomes https://pypi.org/project/scikit-learn/*
    
    Authors:
      - James Lamb (https://github.com/jameslamb)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Alex Barghi (https://github.com/alexbarghi-nv)
    
    URL: https://github.com/rapidsai/cugraph-gnn/pull/67

--- ci/build_wheel_cugraph-dgl.sh
+++ ci/build_wheel_cugraph-dgl.sh
@@ -3,4 +3,7 @@
 
 set -euo pipefail
 
-./ci/build_wheel.sh cugraph-dgl python/cugraph-dgl
+package_dir="python/cugraph-dgl"
+
+./ci/build_wheel.sh cugraph-dgl ${package_dir}
+./ci/validate_wheel.sh ${package_dir} dist
