7e316d9c2af72c1730aaac23308d9af8ea848f96
James Lamb <jlamb@nvidia.com>
2025-01-13 22:20:33 -0600

    fix conda

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -7,8 +7,8 @@ RAFT_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact raft 2531 python "${RAFT_COMM
 
 CUGRAPH_COMMIT="8fe1d33cbcaf1f40a6b3d06ec48cc699c47f8b44"
 
-CUGRAPH_CPP_CHANNEL=$(rapids-get-pr-conda-artifact raft 4804 cpp "${CUGRAPH_COMMIT:0:7}")
-CUGRAPH_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact raft 4804 python "${CUGRAPH_COMMIT:0:7}")
+CUGRAPH_CPP_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4804 cpp "${CUGRAPH_COMMIT:0:7}")
+CUGRAPH_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4804 python "${CUGRAPH_COMMIT:0:7}")
 
 conda config --system --add channels "${RAFT_CPP_CHANNEL}"
 conda config --system --add channels "${RAFT_PYTHON_CHANNEL}"
