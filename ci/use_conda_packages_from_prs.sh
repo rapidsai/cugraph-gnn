e3317542a35ac0332a9fc01ae5eeb9b2d62596fd
James Lamb <jlamb@nvidia.com>
2025-01-14 09:44:46 -0600

    update RAFT and cuGraph commits

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -1,11 +1,11 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 
-RAFT_COMMIT="d275c995fb51310d1340fe2fd6d63d0bfd43cafa"
+RAFT_COMMIT="4b793be27b27d40119706ea5df26cc03c8efe33c"
 
 RAFT_CPP_CHANNEL=$(rapids-get-pr-conda-artifact raft 2531 cpp "${RAFT_COMMIT:0:7}")
 RAFT_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact raft 2531 python "${RAFT_COMMIT:0:7}")
 
-CUGRAPH_COMMIT="8fe1d33cbcaf1f40a6b3d06ec48cc699c47f8b44"
+CUGRAPH_COMMIT="12a01e2cb48cd6018bf9a2332a4975825307fea7"
 
 CUGRAPH_CPP_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4804 cpp "${CUGRAPH_COMMIT:0:7}")
 CUGRAPH_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4804 python "${CUGRAPH_COMMIT:0:7}")
--- ci/use_wheels_from_prs.sh
+++ ci/use_wheels_from_prs.sh
@@ -2,7 +2,7 @@
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
 
-RAFT_COMMIT="d275c995fb51310d1340fe2fd6d63d0bfd43cafa"
+RAFT_COMMIT="4b793be27b27d40119706ea5df26cc03c8efe33c"
 LIBRAFT_CHANNEL=$(
   RAPIDS_PY_WHEEL_NAME="libraft_${RAPIDS_PY_CUDA_SUFFIX}" rapids-get-pr-wheel-artifact raft 2531 cpp "${RAFT_COMMIT:0:7}"
 )
@@ -13,7 +13,7 @@ RAFT_DASK_CHANNEL=$(
   RAPIDS_PY_WHEEL_NAME="raft_dask_${RAPIDS_PY_CUDA_SUFFIX}" rapids-get-pr-wheel-artifact raft 2531 python "${RAFT_COMMIT:0:7}"
 )
 
-CUGRAPH_COMMIT="8fe1d33cbcaf1f40a6b3d06ec48cc699c47f8b44"
+CUGRAPH_COMMIT="12a01e2cb48cd6018bf9a2332a4975825307fea7"
 CUGRAPH_CHANNEL=$(
   RAPIDS_PY_WHEEL_NAME="cugraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-get-pr-wheel-artifact cugraph 4804 python "${CUGRAPH_COMMIT:0:}"
 )
