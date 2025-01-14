f594fbf773dec4af1ce7201974d7a1bd56f9810f
James Lamb <jlamb@nvidia.com>
2025-01-13 22:07:51 -0600

    fix

--- ci/build_wheel_cugraph-dgl.sh
+++ ci/build_wheel_cugraph-dgl.sh
@@ -5,10 +5,6 @@ set -euo pipefail
 
 package_dir="python/cugraph-dgl"
 
-# Download the libcuml wheel built in the previous step and make it
-# available for pip to find.
-LIBCUML_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libcuml_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/libcuml_dist)
-
 source ./ci/use_wheels_from_prs.sh
 
 ./ci/build_wheel.sh cugraph-dgl ${package_dir}
