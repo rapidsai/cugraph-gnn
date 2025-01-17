b8cf94b24cb081b524476db65b5104bbffd33dd6
Bradley Dice <bdice@bradleydice.com>
2025-01-16 18:28:16 -0600

    Update cugraph commit to use published libraft wheels.

--- ci/use_conda_packages_from_prs.sh
+++ ci/use_conda_packages_from_prs.sh
@@ -1,6 +1,6 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
 
-CUGRAPH_COMMIT="12a01e2cb48cd6018bf9a2332a4975825307fea7"
+CUGRAPH_COMMIT="5b6fd89c2e44ef58c22131aa799807151bff4e42"
 
 CUGRAPH_CPP_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4804 cpp "${CUGRAPH_COMMIT:0:7}")
 CUGRAPH_PYTHON_CHANNEL=$(rapids-get-pr-conda-artifact cugraph 4804 python "${CUGRAPH_COMMIT:0:7}")
