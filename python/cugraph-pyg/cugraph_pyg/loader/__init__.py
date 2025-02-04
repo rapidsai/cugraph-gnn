431801cd758720ce0056883bcc4ff0ceca795eca
Alex Barghi <105237337+alexbarghi-nv@users.noreply.github.com>
2025-02-04 12:37:42 -0800

    Deprecate the Dask API in cuGraph-PyG (#118)
    Now that all features supported by the Dask API are available in the new API, we are deprecating the Dask API.  It will be removed in release 25.06.
    
    Merge after #104 
    Closes #86
    
    Authors:
      - Alex Barghi (https://github.com/alexbarghi-nv)
    
    Approvers:
      - Tingyu Wang (https://github.com/tingyu66)
    
    URL: https://github.com/rapidsai/cugraph-gnn/pull/118

--- python/cugraph-pyg/cugraph_pyg/loader/__init__.py
+++ python/cugraph-pyg/cugraph_pyg/loader/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022-2024, NVIDIA CORPORATION.
+# Copyright (c) 2022-2025, NVIDIA CORPORATION.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -19,9 +19,35 @@ from cugraph_pyg.loader.neighbor_loader import NeighborLoader
 from cugraph_pyg.loader.link_loader import LinkLoader
 from cugraph_pyg.loader.link_neighbor_loader import LinkNeighborLoader
 
-from cugraph_pyg.loader.dask_node_loader import DaskNeighborLoader
+from cugraph_pyg.loader.dask_node_loader import (
+    DaskNeighborLoader as DEPRECATED__DaskNeighborLoader,
+)
 
-from cugraph_pyg.loader.dask_node_loader import BulkSampleLoader
+from cugraph_pyg.loader.dask_node_loader import (
+    BulkSampleLoader as DEPRECATED__BulkSampleLoader,
+)
+
+
+def DaskNeighborLoader(*args, **kwargs):
+    warnings.warn(
+        "DaskNeighborLoader and the Dask API are deprecated."
+        "Consider switching to the new API"
+        " (cugraph_pyg.loader.node_loader, cugraph_pyg.loader.link_loader).",
+        FutureWarning,
+    )
+
+    return DEPRECATED__DaskNeighborLoader(*args, **kwargs)
+
+
+def BulkSampleLoader(*args, **kwargs):
+    warnings.warn(
+        "BulkSampleLoader and the Dask API are deprecated.  Unbuffered sampling"
+        " in cugraph_pyg will soon be completely deprecated and removed, including"
+        " in the new API.",
+        FutureWarning,
+    )
+
+    return DEPRECATED__BulkSampleLoader(*args, **kwargs)
 
 
 def CuGraphNeighborLoader(*args, **kwargs):
