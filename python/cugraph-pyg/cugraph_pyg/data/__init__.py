9a675f23feb7a5eceb7d8f4c4054966f31c64d34
Alex Barghi <105237337+alexbarghi-nv@users.noreply.github.com>
2025-03-24 13:14:48 -0700

    Revert "Branch 25.06 forward resolve (#163)"
    This reverts commit 50e769d0c18bc0df9bf4bb2b92ace6dda84cd810.

--- python/cugraph-pyg/cugraph_pyg/data/__init__.py
+++ python/cugraph-pyg/cugraph_pyg/data/__init__.py
@@ -18,20 +18,9 @@ from cugraph_pyg.data.dask_graph_store import (
 )
 from cugraph_pyg.data.graph_store import GraphStore
 from cugraph_pyg.data.feature_store import (
-    TensorDictFeatureStore as DEPRECATED__TensorDictFeatureStore,
+    TensorDictFeatureStore,
     WholeFeatureStore,
 )
-
-
-def TensorDictFeatureStore(*args, **kwargs):
-    warnings.warn(
-        "TensorDictFeatureStore is deprecated.  Consider changing your "
-        "workflow to launch using 'torchrun' and store data in "
-        "the faster and more memory-efficient WholeFeatureStore instead.",
-        FutureWarning,
-    )
-
-    return DEPRECATED__TensorDictFeatureStore(*args, **kwargs)
 
 
 def DaskGraphStore(*args, **kwargs):
