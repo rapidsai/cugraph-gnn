8c4c9648713e8c3947cd920a4b4cb68f5db0f2b6
Gil Forsyth <gforsyth@nvidia.com>
2025-04-17 11:42:16 -0400

    refactor(pylibwholegraph): pull down libwholegraph wheel before build

--- ci/build_wheel_pylibwholegraph.sh
+++ ci/build_wheel_pylibwholegraph.sh
@@ -5,6 +5,20 @@ set -euo pipefail
 
 package_dir="python/pylibwholegraph"
 
+
+RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
+# Download the libcugraph wheel built in the previous step and make it
+# available for pip to find.
+LIBWHOLEGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github cpp /tmp/libwholegraph_dist)
+
+cat >> ./constraints.txt <<EOF
+libwholegraph-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBWHOLEGRAPH_WHEELHOUSE}"/libwholegraph_*.whl)
+EOF
+
+# Using env variable PIP_CONSTRAINT is necessary to ensure the constraints
+# are used when creating the isolated build environment.
+export PIP_CONSTRAINT="${PWD}/constraints.txt"
+
 export SKBUILD_CMAKE_ARGS="-DBUILD_SHARED_LIBS=OFF;-DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE;-DCUDA_STATIC_RUNTIME=ON;-DWHOLEGRAPH_BUILD_WHEELS=ON"
 
 ./ci/build_wheel.sh pylibwholegraph ${package_dir}
