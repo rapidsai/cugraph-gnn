d3a5be519464de61649444d4fc891c778d126ed2
Gil Forsyth <gforsyth@nvidia.com>
2025-04-18 10:02:27 -0400

    chore: set -x to debug pylibwholegraph

--- ci/build_wheel_pylibwholegraph.sh
+++ ci/build_wheel_pylibwholegraph.sh
@@ -6,20 +6,21 @@ set -euo pipefail
 package_dir="python/pylibwholegraph"
 
 
+set -x
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")"
+
 # Download the libcugraph wheel built in the previous step and make it
 # available for pip to find.
-LIBWHOLEGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/libwholegraph_dist)
-
-cat >> ./constraints.txt <<EOF
-libwholegraph-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBWHOLEGRAPH_WHEELHOUSE}"/libwholegraph_*.whl)
-EOF
+RAPIDS_PY_WHEEL_NAME="libwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-s3 cpp /tmp/libwholegraph_dist
+echo "libwholegraph-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo /tmp/libwholegraph_dist/libwholegraph_*.whl)" > /tmp/constraints.txt
 
 # Using env variable PIP_CONSTRAINT is necessary to ensure the constraints
 # are used when creating the isolated build environment.
-export PIP_CONSTRAINT="${PWD}/constraints.txt"
+export PIP_CONSTRAINT="/tmp/constraints.txt"
 
 export SKBUILD_CMAKE_ARGS="-DBUILD_SHARED_LIBS=OFF;-DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE;-DCUDA_STATIC_RUNTIME=ON;-DWHOLEGRAPH_BUILD_WHEELS=ON"
 
 ./ci/build_wheel.sh pylibwholegraph ${package_dir}
 ./ci/validate_wheel.sh ${package_dir} "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+set +x
