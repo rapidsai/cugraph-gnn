8f04a3041a0073eccf66ea1516979f31db9bbb8f
Gil Forsyth <gforsyth@nvidia.com>
2025-05-12 14:38:52 -0400

    revert changes to dgl pytests

--- ci/run_cugraph_dgl_pytests.sh
+++ ci/run_cugraph_dgl_pytests.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Copyright (c) 2024-2025, NVIDIA CORPORATION.
+# Copyright (c) 2024, NVIDIA CORPORATION.
 
 set -euo pipefail
-
 
 # Support invoking run_cugraph_dgl_pytests.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../python/cugraph-dgl/cugraph_dgl
