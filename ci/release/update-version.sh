7520f1488ce549129814687cc3ff3038bb7177ac
Alexandria Barghi <abarghi@nvidia.com>
2025-05-19 11:12:36 -0700

    remove cugraph-DGL

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -49,7 +49,6 @@ NEXT_SHORT_TAG_PEP440=$(python -c "from packaging.version import Version; print(
 DEPENDENCIES=(
   cudf
   cugraph
-  cugraph-dgl
   cugraph-pyg
   dask-cuda
   dask-cudf
@@ -65,7 +64,7 @@ DEPENDENCIES=(
   rapids-dask-dependency
 )
 for DEP in "${DEPENDENCIES[@]}"; do
-  for FILE in dependencies.yaml conda/environments/*.yaml python/cugraph-{pyg,dgl}/conda/*.yaml; do
+  for FILE in dependencies.yaml conda/environments/*.yaml python/cugraph-{pyg}/conda/*.yaml; do
     sed_runner "/-.* ${DEP}\(-cu[[:digit:]]\{2\}\)\{0,1\}==/ s/==.*/==${NEXT_SHORT_TAG_PEP440}.*,>=0.0.0a0/g" "${FILE}"
   done
   for FILE in python/**/pyproject.toml; do
--- ci/test_python.sh
+++ ci/test_python.sh
@@ -6,12 +6,6 @@ set -euo pipefail
 # Support invoking test_python.sh outside the script directory
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"/../
 
-if [[ "${RAPIDS_CUDA_VERSION%%.*}" == "11" ]]; then
-  DGL_CHANNEL="dglteam/label/th24_cu118"
-else
-  DGL_CHANNEL="dglteam/label/th24_cu124"
-fi
-
 . /opt/conda/etc/profile.d/conda.sh
 
 RAPIDS_VERSION="$(rapids-version)"
@@ -34,59 +28,6 @@ popd
 EXITCODE=0
 trap "EXITCODE=1" ERR
 set +e
-
-# Test runs that include tests that use dask require
-# --import-mode=append. Those tests start a LocalCUDACluster that inherits
-# changes from pytest's modifications to PYTHONPATH (which defaults to
-# prepending source tree paths to PYTHONPATH).  This causes the
-# LocalCUDACluster subprocess to import cugraph from the source tree instead of
-# the install location, and in most cases, the source tree does not have
-# extensions built in-place and will result in ImportErrors.
-#
-# FIXME: TEMPORARILY disable MG PropertyGraph tests (experimental) tests and
-# bulk sampler IO tests (hangs in CI)
-
-if [[ "${RUNNER_ARCH}" != "ARM64" ]]; then
-  rapids-logger "(cugraph-dgl) Generate Python testing dependencies"
-  rapids-dependency-file-generator \
-    --output conda \
-    --file-key test_cugraph_dgl \
-    --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}"  \
-    --prepend-channel "${CPP_CHANNEL}" \
-    --prepend-channel "${PYTHON_CHANNEL}" \
-    --prepend-channel conda-forge \
-    --prepend-channel "${DGL_CHANNEL}" \
-    --prepend-channel nvidia \
-  | tee env.yaml
-
-  rapids-mamba-retry env create --yes -f env.yaml -n test_cugraph_dgl
-
-  # activate test_cugraph_dgl environment for dgl
-  set +u
-  conda activate test_cugraph_dgl
-  set -u
-
-
-  rapids-print-env
-
-  rapids-logger "Check GPU usage"
-  nvidia-smi
-
-  rapids-logger "pytest cugraph_dgl (single GPU)"
-  ./ci/run_cugraph_dgl_pytests.sh \
-    --junitxml="${RAPIDS_TESTS_DIR}/junit-cugraph-dgl.xml" \
-    --cov-config=../../.coveragerc \
-    --cov=cugraph_dgl \
-    --cov-report=xml:"${RAPIDS_COVERAGE_DIR}/cugraph-dgl-coverage.xml" \
-    --cov-report=term
-
-  # Reactivate the test environment back
-  set +u
-  conda deactivate
-  set -u
-else
-  rapids-logger "skipping cugraph_dgl pytest on ARM64"
-fi
 
 if [[ "${RUNNER_ARCH}" != "ARM64" ]]; then
   rapids-logger "(cugraph-pyg) Generate Python testing dependencies"
