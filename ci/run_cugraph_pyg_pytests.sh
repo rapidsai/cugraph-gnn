42235c3dea6f23260ce2f7e9311e759ba143f97b
Alexandria Barghi <abarghi@nvidia.com>
2025-06-11 15:55:07 -0700

    remove rapids-logger usage from pytest script

--- ci/run_cugraph_pyg_pytests.sh
+++ ci/run_cugraph_pyg_pytests.sh
@@ -16,10 +16,10 @@ export TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD=1
 
 # Test examples (disabled due to excessive network bandwidth usage)
 for e in "$(pwd)"/examples/*.py; do
-  rapids-logger "running example $e"
+  echo "running example $e"
   (yes || true) | torchrun --nnodes 1 --nproc_per_node 1 $e --dataset_root "${RAPIDS_DATASET_ROOT_DIR}/ogb_datasets"
 done
 
-rapids-logger "running bitcoin example"
+echo "running bitcoin example"
 (yes || true) | torchrun --nnodes 1 --nproc_per_node 1 "$(pwd)"/examples/fraud/bitcoin_mnmg.py --dataset_root "${RAPIDS_DATASET_ROOT_DIR}" --embedding_dir "${RAPIDS_DATASET_ROOT_DIR}/bitcoin_embeddings"
 python "$(pwd)"/examples/fraud/bitcoin_rf.py --dataset_root "${RAPIDS_DATASET_ROOT_DIR}" --embedding_dir "${RAPIDS_DATASET_ROOT_DIR}/bitcoin_embeddings"
