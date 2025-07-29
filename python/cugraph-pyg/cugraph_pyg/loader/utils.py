12a5adfd8bdbeb8727c5c06a8e9d3b503a844080
Alexandria Barghi <abarghi@nvidia.com>
2025-07-29 10:31:38 -0700

    switch to int64

--- python/cugraph-pyg/cugraph_pyg/loader/utils.py
+++ python/cugraph-pyg/cugraph_pyg/loader/utils.py
@@ -19,10 +19,10 @@ def generate_seed():
     rank = torch.distributed.get_rank()
     if rank == 0:
         seed = torch.randint(
-            0, 2**64 - world_size - 1, (1,), dtype=torch.uint64, device="cuda"
+            0, 2**63 - world_size - 1, (1,), dtype=torch.int64, device="cuda"
         )
     else:
-        seed = torch.tensor([0], dtype=torch.uint64, device="cuda")
+        seed = torch.tensor([0], dtype=torch.int64, device="cuda")
     torch.distributed.broadcast(seed, src=0)
     seed = seed.item() + rank
     return seed
