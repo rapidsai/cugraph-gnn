90b6356bb2e337ad207e6be288215a41d0905776
Alexandria Barghi <abarghi@nvidia.com>
2025-07-31 12:31:25 -0700

    change max to 2**63 - world_size

--- python/cugraph-pyg/cugraph_pyg/loader/utils.py
+++ python/cugraph-pyg/cugraph_pyg/loader/utils.py
@@ -19,7 +19,7 @@ def generate_seed():
     rank = torch.distributed.get_rank()
     if rank == 0:
         seed = torch.randint(
-            0, 2**63 - world_size - 1, (1,), dtype=torch.int64, device="cuda"
+            0, 2**63 - world_size, (1,), dtype=torch.int64, device="cuda"
         )
     else:
         seed = torch.tensor([0], dtype=torch.int64, device="cuda")
