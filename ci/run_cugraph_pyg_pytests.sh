8532f9f50b3a8d9cca648ba31a13f6e2082a88ee
Alexandria Barghi <abarghi@nvidia.com>
2025-08-14 14:14:12 -0700

    disable example tests

--- ci/run_cugraph_pyg_pytests.sh
+++ ci/run_cugraph_pyg_pytests.sh
@@ -14,11 +14,11 @@ export CI=true
 # Enable legacy behavior of torch.load for examples relying on ogb
 export TORCH_FORCE_NO_WEIGHTS_ONLY_LOAD=1
 
-# Test examples (disabled due to excessive network bandwidth usage)
-for e in "$(pwd)"/examples/*.py; do
-  echo "running example $e"
-  (yes || true) | torchrun --nnodes 1 --nproc_per_node 1 $e --dataset_root "${RAPIDS_DATASET_ROOT_DIR}/ogb_datasets"
-done
+# Test examples (disabled due to lack of memory)
+#for e in "$(pwd)"/examples/*.py; do
+#  echo "running example $e"
+#  (yes || true) | torchrun --nnodes 1 --nproc_per_node 1 $e --dataset_root "${RAPIDS_DATASET_ROOT_DIR}/ogb_datasets"
+#done
 
 # echo "running bitcoin example"
 # (yes || true) | torchrun --nnodes 1 --nproc_per_node 1 "$(pwd)"/examples/fraud/bitcoin_mnmg.py --dataset_root "${RAPIDS_DATASET_ROOT_DIR}" --embedding_dir "${RAPIDS_DATASET_ROOT_DIR}/bitcoin_embeddings"
