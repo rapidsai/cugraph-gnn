d4b52c9ca8183ea5cfb5301113e4f211e4168f6b
Alex Barghi <105237337+alexbarghi-nv@users.noreply.github.com>
2025-09-24 14:31:24 -0700

    [FEA] Enable Temporal Sampling in cuGraph-PyG (#310)
    Enables temporal sampling in cuGraph-PyG with some constraints:
    1. Only supports sampling forward in time (probably an API violation)
    2. Does not support time overrides (definitely an API violation)
    
    Closes #116
    
    Authors:
      - Alex Barghi (https://github.com/alexbarghi-nv)
    
    Approvers:
      - Tingyu Wang (https://github.com/tingyu66)
    
    URL: https://github.com/rapidsai/cugraph-gnn/pull/310

--- python/cugraph-pyg/cugraph_pyg/sampler/sampler.py
+++ python/cugraph-pyg/cugraph_pyg/sampler/sampler.py
@@ -367,6 +367,11 @@ class HeterogeneousSampleReader(SampleReader):
                     )
 
             if input_type == pyg_can_etype:
+                # FIXME this is technically not the correct way to calculate the
+                # number of sampled nodes for hop 0, but it should not cause any
+                # issues during training since (presumably) the extra nodes will
+                # get pruned out (rapidsai/cugraph#5270).
+
                 integer_input_type = etype
                 # heterogeneous edges as input, two node types per edge type
                 ux = col[pyg_can_etype][: num_sampled_edges[pyg_can_etype][0]]
@@ -376,6 +381,7 @@ class HeterogeneousSampleReader(SampleReader):
                     if ux.numel() > 0
                     else torch.tensor(0, device=ux.device)
                 )
+
                 num_sampled_nodes[self.__dst_types[etype]][0] = torch.max(
                     num_sampled_nodes[self.__dst_types[etype]][0],
                     uxn.reshape((1,)),
@@ -385,6 +391,7 @@ class HeterogeneousSampleReader(SampleReader):
                     if uy.numel() > 0
                     else torch.tensor(0, device=uy.device)
                 )
+
                 num_sampled_nodes[self.__src_types[etype]][0] = torch.max(
                     num_sampled_nodes[self.__src_types[etype]][0],
                     uyn.reshape((1,)),
