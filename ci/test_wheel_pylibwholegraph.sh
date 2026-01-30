d83f5728cd5ba50ff67f41f0c16b8a748dc32d26
James Lamb <jlamb@nvidia.com>
2026-01-30 11:01:21 -0600

    tighten wheel size limits, expand CI-skipping logic, other small build changes

--- ci/test_wheel_pylibwholegraph.sh
+++ ci/test_wheel_pylibwholegraph.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION.
+# SPDX-FileCopyrightText: Copyright (c) 2023-2026, NVIDIA CORPORATION.
 # SPDX-License-Identifier: Apache-2.0
 
 set -e          # abort the script on error
@@ -12,6 +12,8 @@ if [[ "${CI:-}" == "true" ]]; then
   rm -rf /usr/lib64/libnccl*
 fi
 
+RAPIDS_INIT_PIP_REMOVE_NVIDIA_INDEX="true"
+export RAPIDS_INIT_PIP_REMOVE_NVIDIA_INDEX
 source rapids-init-pip
 
 RAPIDS_PY_CUDA_SUFFIX="$(rapids-wheel-ctk-name-gen ${RAPIDS_CUDA_VERSION})"
