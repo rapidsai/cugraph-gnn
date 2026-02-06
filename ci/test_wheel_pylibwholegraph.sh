23a67e360f9e42030799537a4e130236d7fc1e45
Bradley Dice <bdice@bradleydice.com>
2026-02-05 22:48:50 -0600

    Use CUDA 13 index on CUDA 13

--- ci/test_wheel_pylibwholegraph.sh
+++ ci/test_wheel_pylibwholegraph.sh
@@ -19,7 +19,11 @@ LIBWHOLEGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="libwholegraph_${RAPIDS_PY_CUDA_
 PYLIBWHOLEGRAPH_WHEELHOUSE=$(RAPIDS_PY_WHEEL_NAME="pylibwholegraph_${RAPIDS_PY_CUDA_SUFFIX}" rapids-download-wheels-from-github python)
 
 # determine pytorch source
-PYTORCH_INDEX_URL="https://download.pytorch.org/whl/cu126"
+if [[ "${CUDA_MAJOR}" == "12" ]]; then
+  PYTORCH_INDEX="https://download.pytorch.org/whl/cu126"
+else
+  PYTORCH_INDEX="https://download.pytorch.org/whl/cu130"
+fi
 RAPIDS_TESTS_DIR=${RAPIDS_TESTS_DIR:-"${PWD}/test-results"}
 RAPIDS_COVERAGE_DIR=${RAPIDS_COVERAGE_DIR:-"${PWD}/coverage-results"}
 mkdir -p "${RAPIDS_TESTS_DIR}" "${RAPIDS_COVERAGE_DIR}"
