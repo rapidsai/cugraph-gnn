0cf9c654ab969aa68f138492fdd3412e7b7ea9d0
Gil Forsyth <gforsyth@nvidia.com>
2026-02-06 14:47:03 -0500

    feat: build wheels with limited API

--- ci/build_wheel_pylibwholegraph.sh
+++ ci/build_wheel_pylibwholegraph.sh
@@ -20,5 +20,13 @@ echo "libwholegraph-${RAPIDS_PY_CUDA_SUFFIX} @ file://$(echo "${LIBWHOLEGRAPH_WH
 
 export SKBUILD_CMAKE_ARGS="-DBUILD_SHARED_LIBS=ON;-DCMAKE_MESSAGE_LOG_LEVEL=VERBOSE"
 
-./ci/build_wheel.sh pylibwholegraph ${package_dir} python
+# TODO: move this variable into `ci-wheel`
+# Format Python limited API version string
+RAPIDS_PY_API="cp${RAPIDS_PY_VERSION//./}"
+export RAPIDS_PY_API
+
+./ci/build_wheel.sh pylibwholegraph ${package_dir} --stable
 ./ci/validate_wheel.sh ${package_dir} "${RAPIDS_WHEEL_BLD_OUTPUT_DIR}"
+
+RAPIDS_PACKAGE_NAME="$(rapids-package-name wheel_python pylibwholegraph --stable --cuda "$RAPIDS_CUDA_VERSION")"
+export RAPIDS_PACKAGE_NAME
