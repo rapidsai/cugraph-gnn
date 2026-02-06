cb29a3891688cef0bed27a8efcada0001eb902ec
Gil Forsyth <gforsyth@nvidia.com>
2026-02-06 15:00:11 -0500

    feat(noarch): build cugraph-pyg as a conda `noarch` package

--- ci/build_python_noarch.sh
+++ ci/build_python_noarch.sh
@@ -12,6 +12,7 @@ export CMAKE_GENERATOR=Ninja
 rapids-print-env
 
 CPP_CHANNEL=$(rapids-download-conda-from-github cpp)
+PYTHON_CHANNEL=$(rapids-download-from-github "$(rapids-package-name conda_python pylibwholegraph --stable --cuda)")
 
 rapids-generate-version > ./VERSION
 
@@ -21,19 +22,19 @@ export RAPIDS_PACKAGE_VERSION
 # populates `RATTLER_CHANNELS` array and `RATTLER_ARGS` array
 source rapids-rattler-channel-string
 
-rapids-logger "Prepending channel ${CPP_CHANNEL} to RATTLER_CHANNELS"
+rapids-logger "Prepending channel ${CPP_CHANNEL} and ${PYTHON_CHANNEL} to RATTLER_CHANNELS"
 
-RATTLER_CHANNELS=("--channel" "${CPP_CHANNEL}" "${RATTLER_CHANNELS[@]}")
+RATTLER_CHANNELS=("--channel" "${CPP_CHANNEL}" "--channel" "${PYTHON_CHANNEL}" "${RATTLER_CHANNELS[@]}")
 
 # TODO: Remove `--test skip` flags once importing on a CPU node works correctly
 sccache --stop-server 2>/dev/null || true
 
-rapids-logger "Building pylibwholegraph"
+rapids-logger "Building cugraph-pyg"
 
 # --no-build-id allows for caching with `sccache`
 # more info is available at
 # https://rattler.build/latest/tips_and_tricks/#using-sccache-or-ccache-with-rattler-build
-rattler-build build --recipe conda/recipes/pylibwholegraph \
+rattler-build build --recipe conda/recipes/cugraph-pyg \
                     --test skip \
                     "${RATTLER_ARGS[@]}" \
                     "${RATTLER_CHANNELS[@]}"
@@ -44,3 +45,6 @@ sccache --stop-server >/dev/null 2>&1 || true
 # remove build_cache directory to avoid uploading the entire source tree
 # tracked in https://github.com/prefix-dev/rattler-build/issues/1424
 rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
+
+RAPIDS_PACKAGE_NAME="$(rapids-package-name conda_python cugraph_pyg --pure)"
+export RAPIDS_PACKAGE_NAME
