a60ae615deae726a5598c8998a1d755b62e2969e
Gil Forsyth <gforsyth@nvidia.com>
2026-02-10 16:21:21 -0500

    feat(conda): build `pylibwholegraph` using limited API

--- ci/build_python.sh
+++ ci/build_python.sh
@@ -44,3 +44,6 @@ sccache --stop-server >/dev/null 2>&1 || true
 # remove build_cache directory to avoid uploading the entire source tree
 # tracked in https://github.com/prefix-dev/rattler-build/issues/1424
 rm -rf "$RAPIDS_CONDA_BLD_OUTPUT_DIR"/build_cache
+
+RAPIDS_PACKAGE_NAME="$(rapids-package-name conda_python pylibwholegraph --stable --cuda)"
+export RAPIDS_PACKAGE_NAME
