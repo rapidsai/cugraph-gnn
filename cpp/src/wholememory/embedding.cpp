fb0af892aed7e24db691fecfb19d41e93cc7f6fc
linhu-nv <linhu@nvidia.com>
2026-04-27 09:15:45 +0800

    support embedding training with bf16 and fp16

--- cpp/src/wholememory/embedding.cpp
+++ cpp/src/wholememory/embedding.cpp
@@ -47,8 +47,10 @@ wholememory_error_code_t embedding_base::set_optimizer(wholememory_embedding_opt
     }
     optimizer = opt;
     if (optimizer != nullptr) {
-      if (embedding_dtype_ != WHOLEMEMORY_DT_FLOAT) {
-        WHOLEMEMORY_ERROR("Only float embedding supports training.");
+      if (embedding_dtype_ != WHOLEMEMORY_DT_FLOAT &&
+          embedding_dtype_ != WHOLEMEMORY_DT_HALF &&
+          embedding_dtype_ != WHOLEMEMORY_DT_BF16) {
+        WHOLEMEMORY_ERROR("Only float, half and bf16 embeddings support training.");
         return WHOLEMEMORY_NOT_IMPLEMENTED;
       }
       if (cache_policy != nullptr) {
@@ -360,6 +362,7 @@ wholememory_error_code_t embedding_base::create_optimizer_states() noexcept
       all_state_embedding_count += aligned_embedding_dim;
     }
     cachable_state_desc            = *user_tensor_desc;
+    cachable_state_desc.dtype      = WHOLEMEMORY_DT_FLOAT;
     cachable_state_desc.sizes[1]   = all_state_embedding_count;
     cachable_state_desc.strides[0] = all_state_embedding_count;
     auto allocated_handle          = wholememory_tensor_get_memory_handle(allocated_embedding);
