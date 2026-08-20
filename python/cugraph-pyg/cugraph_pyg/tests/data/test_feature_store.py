1fe92f6ce712da13515346c3757ff4716f163f55
Alexandria Barghi <abarghi@nvidia.com>
2026-08-20 14:50:29 -0700

    fix style

--- python/cugraph-pyg/cugraph_pyg/tests/data/test_feature_store.py
+++ python/cugraph-pyg/cugraph_pyg/tests/data/test_feature_store.py
@@ -44,9 +44,7 @@ def test_feature_store_basic_api(single_pytorch_worker):
     assert len(feature_store.get_all_tensor_attrs()) == 3
 
     dist_tensor = feature_store["node", "feat1", None]
-    warning = (
-        "Putting a DistTensor into a FeatureStore performs a zero-copy operation"
-    )
+    warning = "Putting a DistTensor into a FeatureStore performs a zero-copy operation"
     with pytest.warns(UserWarning, match=warning):
         feature_store["node", "feat1_alias", None] = dist_tensor
     assert feature_store["node", "feat1_alias", None] is dist_tensor
